8d9d94ddf9359252603dadaa48c6b9b0beeb5fc1
Tiago-Lucas <lucastiago45@gmail.com>
2020-11-05 17:24:29 -0200

    Projeto Criado

---
+++ src/main/java/com/br/FeiraOnline/FeiraOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.br.FeiraOnline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeiraOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeiraOnlineApplication.class, args);
+	}
+
+}
